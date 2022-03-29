75b36882323edff437edfb0d2e280193edd8089d
Suheda Karabudak <karabudaksuheda@gmail.com>
2022-03-29 23:05:02 +0300

    algoritm commit

---
+++ NarcissisticNumber.py
@@ -0,0 +1,15 @@
+#A Narcissistic Number is a positive number which is the sum of its own digits,
+#each raised to the power of the number of digits in a given base.
+# In this Kata, we will restrict ourselves to decimal (base 10).
+number=int(input("Enter a number: "))
+
+basamak=len(str(number))
+
+sum=0
+num=list(str(number))
+for i in num:
+    sum=int(i)**basamak +sum
+if number==sum:
+    print("{} narcissistics are  number. ".format(number))
+else:
+    print("{} narcissistics are not number".format(number))
